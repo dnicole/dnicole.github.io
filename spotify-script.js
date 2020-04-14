08ee36948b06f5f886a25e1e02b718dd9e19ffb3
Nicole Pank <nicole.pank@gmail.com>
2020-04-13 22:38:46 -0500

    summary

--- spotify-script.js
+++ spotify-script.js
@@ -1,25 +1,26 @@
-document = "/spotify.html";
+(function() {
 
-var loginButton = document.getElementById('button-login');
+	function getUserData(accessToken) {
+	  return $.ajax({
+		  url: 'https://api.spotify.com/v1/me',
+		  headers: {
+			 'Authorization': 'Bearer ' + accessToken
+		  }
+	  });
+	};
 
-loginButton.addEventListener('click', function() {
-	alert("step 1")
-	login(function(accessToken) {
-		alert("step 2")
-		getUserData(accessToken)
-			.then(function(response) {
-				alert(response);
-				// loginButton.style.display = 'none';
-				// resultsPlaceholder.innerHTML = template(response);
+	var loginButton = document.getElementById('button-login');
+	loginButton.addEventListener('click', function(event) {
+		alert("step 1")
+		login(function(accessToken) {
+			alert("step 2")
+			getUserData(accessToken)
+				.then(function(response) {
+					alert(response);
+					// loginButton.style.display = 'none';
+					// resultsPlaceholder.innerHTML = template(response);
+			});
 		});
-	});
-}, passiveSupported ? { passive: true } : false);
+	}, passiveSupported ? { passive: true } : false);
 
-function getUserData(accessToken) {
-  return $.ajax({
-	  url: 'https://api.spotify.com/v1/me',
-	  headers: {
-		 'Authorization': 'Bearer ' + accessToken
-	  }
-  });
-}
+})();
